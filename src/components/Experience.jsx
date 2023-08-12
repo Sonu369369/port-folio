8766508f73ac58e6e8aaec6402cda29f37c92357
Ankit <2002ankit2002@gmail.com>
2023-08-12 17:02:36 +0530

    upgrading

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -3,11 +3,12 @@ import React from "react";
 import html from "../assets/html.png";
 import css from "../assets/css.png";
 import javascript from "../assets/javascript.png";
+import node from "../assets/node.png";
 import reactImage from "../assets/react.png";
-import nextjs from "../assets/nextjs.png";
-import graphql from "../assets/graphql.png";
 import github from "../assets/github.png";
 import tailwind from "../assets/tailwind.png";
+import mongo from "../assets/mongo.png";
+import express from "../assets/express.png";
 
 const Experience = () => {
   const techs = [
@@ -43,20 +44,26 @@ const Experience = () => {
     },
     {
       id: 6,
-      src: nextjs,
-      title: "Next JS",
+      src: mongo,
+      title: "Mongo Db",
       style: "shadow-white",
     },
     {
       id: 7,
-      src: graphql,
-      title: "GraphQL",
+      src: express,
+      title: "Express",
       style: "shadow-pink-400",
     },
     {
       id: 8,
       src: github,
       title: "GitHub",
+      style: "shadow-gray-400",
+    },
+    {
+      id: 9,
+      src: node,
+      title: "Node.js",
       style: "shadow-gray-400",
     },
   ];
@@ -67,8 +74,8 @@ const Experience = () => {
       className="bg-gradient-to-b from-gray-800 to-black w-full h-screen"
     >
       <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
-        <div>
-          <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">
+        <div className="pt-20">
+          <p className="text-4xl font-bold border-b-4 border-gray-500 p-2  inline">
             Experience
           </p>
           <p className="py-6">These are the technologies I've worked with</p>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import HeroImage from "../assets/heroImage.png";
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 import { Link } from "react-scroll";
 
 const Home = () => {
+  const [isImageHovered, setIsImageHovered] = useState(false);
+
   return (
     <div
       name="home"
@@ -15,9 +17,9 @@ const Home = () => {
             I'm a Full Stack Developer
           </h2>
           <p className="text-gray-500 py-4 max-w-md">
-            I have 8 years of experience building and desgining software.
-            Currently, I love to work on web application using technologies like
-            React, Tailwind, Next JS and GraphQL.
+            I have 6 month of experience building and designing software.
+            Currently, I love to work on web applications using technologies
+            like React, Tailwind, Next.js, and GraphQL.
           </p>
 
           <div>
@@ -35,12 +37,46 @@ const Home = () => {
           </div>
         </div>
 
-        <div>
+        <div
+          className={`relative overflow-hidden rounded-2xl ${
+            isImageHovered ? "shadow-lg" : "shadow-md"
+          } transition-shadow duration-300`}
+          onMouseEnter={() => setIsImageHovered(true)}
+          onMouseLeave={() => setIsImageHovered(false)}
+        >
           <img
             src={HeroImage}
             alt="my profile"
-            className="rounded-2xl mx-auto w-2/3 md:w-full"
+            className={`w-2/3 md:w-full ${
+              isImageHovered ? "scale-105" : "scale-100"
+            } transition-transform duration-300 ${
+              isImageHovered ? "opacity-80" : "opacity-100"
+            }`}
+            style={{
+              animation: "fadeIn 1s ease-in-out",
+              transformOrigin: "center",
+              cursor: "pointer",
+            }}
           />
+          {isImageHovered && (
+            <div
+              className="absolute inset-0 rounded-2xl border-4"
+              style={{
+                borderImage: "linear-gradient(45deg, #f06, #9f6) 1",
+                animation: "borderGradientAnimation 1s infinite alternate",
+              }}
+            />
+          )}
+          {isImageHovered && (
+            <div
+              className="absolute inset-0 rounded-2xl border-4"
+              style={{
+                borderImage: "linear-gradient(45deg, #9f6, #06f) 1",
+                animation:
+                  "borderGradientAnimationHover 0.5s infinite alternate",
+              }}
+            />
+          )}
         </div>
       </div>
     </div>
